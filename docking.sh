e6bd5e0f5a4fe12e5e2bb138f4a024003fb7917f
k-eeer <ka7003te@gmail.com>
2023-04-05 14:25:37 +0800

    加入詢問使用者輸入本次準備及使用藥物分子數量環節

--- docking.sh
+++ docking.sh
@@ -9,6 +9,7 @@ if [[ ! -f "protein$file" ]]; then
 	#frame1 of correctNma.pdb as reference.
 	grep -A 589  "MODEL        1" correctNma.pdb>frame1.pdb
 	#cluster threshold 0.26nm to get 3 receptor;use Gromacs"
+	#先用完整分子(3cm5)以chimera:match maker對齊clNmas*.pdb後其3cm5 model寫成pdb
 echo $'0\n0\n'|gmx cluster -f correctNma.pdb  -s frame1.pdb  -method gromos -cl clNma.pdb  -cutoff 0.26 -pbc
 
 	#split PDBs to single PDB.
@@ -17,8 +18,12 @@ echo $'0\n0\n'|gmx cluster -f correctNma.pdb  -s frame1.pdb  -method gromos -cl 
 	#superposition.;file:forAlignPml.sh;tool:Pymol
 	#bash forAlignPml.sh
 	#pymol -c segmentAlign.pml 1>/dev/null
+	#cluster之後
+	#cluster後chainID遺失，目前
+	#先用完整分子以chimera:match maker對齊clNmas*.pdb後寫成protein*.pdb
+	
 	for i in {1..3};do
-		mv cluster$i.pdb protein$i.pdb
+		#mv cluster$i.pdb protein$i.pdb
 		prepare_receptor4.py -r protein$i.pdb  -o protein$i.pdbqt -A hydrogens
 	done
 fi
@@ -42,64 +47,60 @@ echo "1-b)準備藥物分子(fda approved drug molecules.)"
 #check number of molecules of download mol2 file. 
 countDrug=drug*pdbqt
 if [[ ! -f $countDrug ]]; then
-
-	echo -n "total molecules you download is "
-	grep '^ZINC' fda.mol2|wc -l
+	drugMol2=$(grep '^ZINC' fda.mol2|wc -l)
+	echo -n total molecules you download is  $drugMol2
 	obabel -imol2 fda.mol2 -opdb -Odrug.pdb -m
 fi
 
-count=$(ls drug*pdbqt|wc -l)
-if [[ $count<30 ]];then
-	#get 1 random ligand for docking.
-	randDrug=$(ls drug*pdb |shuf -n 1)
-	for l in $randDrug;do
-		prepare_ligand4.py -l ${l}  -o `basename ${l} .pdb`.pdbqt 1>/dev/null
-		rm -f $l
-	done
+
+
+#get  random prepared drug for docking.
+read -p 請輸入想要準備的藥物分子數量(1-$drugMol2) drugPrepareNum
+randDrug=$(ls drug*pdb |shuf -n $drugPrepareNum)
+for l in $randDrug;do
+	prepare_ligand4.py -l ${l}  -o `basename ${l} .pdb`.pdbqt 1>/dev/null
+	rm -f $l
+done
 #---------------------------------------------------------------------
 #2)蛋白質分子藥物分子對接
-	echo "2)蛋白質分子藥物分子對接"
-	rm -f dockingAffinity.txt 
-	rm -f out*
-	for r in protein*.pdbqt;do
-		#單純測試，因此每個蛋白分子都只試接1藥物分子
-		for l in $(shuf -e -n1 drug*pdbqt);do
-			echo "${r} - ${l}對接"
-			smina --receptor $r --ligand $l --center_x $cx --center_y $cy --center_z $cz --size_x 20 --size_y 20 --size_z 20 --num_modes 1 --out out`basename ${r} .pdbqt`${l}  1>/dev/null
-			echo -n out`basename ${r} .pdbqt`${l} "">> dockingAffinity.txt
-			grep minimizedAffinity out`basename ${r} .pdbqt`${l}|awk '{print $3}'>> dockingAffinity.txt
-		done
+echo "2)蛋白質分子藥物分子對接"
+rm -f dockingAffinity.txt 
+rm -f out*
+for r in protein*.pdbqt;do
+	preparedDrugNow=$(ls drug*pdbqt|wc -l)
+	echo -p 這次希望每個蛋白結構分別嘗試對接幾個藥物分子？(1-$preparedDrugNow) dockingDrugNumThisTime
+	for l in $(shuf -e -n1 $dockingDrugNumThisTime);do
+		echo "${r} - ${l}對接"
+		smina --receptor $r --ligand $l --center_x $cx --center_y $cy --center_z $cz --size_x 20 --size_y 20 --size_z 20 --num_modes 1 --out out`basename ${r} .pdbqt`${l}  1>/dev/null
+		echo -n out`basename ${r} .pdbqt`${l} "">> dockingAffinity.txt
+		grep minimizedAffinity out`basename ${r} .pdbqt`${l}|awk '{print $3}'>> dockingAffinity.txt
 	done
+done
 
 #-----------------------------------------------------------------------
 #3)重新評估結果
 
-	echo "3)重新評估以得更可靠結果："
-	rm -f sortDocking.txt
-	sort -o sortDocking.txt -gk2 dockingAffinity.txt
-	rm -f rescoring.txt
-	top=$(less sortDocking.txt|tail -n 3|awk '{print $1}')
-	for r in protein*.pdbqt;do
-		for s in $top;do
-			r1=`basename ${r} .pdbqt`
-			s1=$(echo ${s:3})
-			s2=${r1}${s1}
-			sed -i  '/MODEL/d' ${s}
-			sed -i '/ENDMDL/d' ${s}
-		
-			echo "rescoring $r ${s} "
-			smina --receptor $r --ligand ${s} --center_x $cx --center_y $cy --center_z $cz --size_x 20 --size_y 20 --size_z 20 --num_modes 1 --scoring vinardo --score_only --out res${s2}   1>/dev/null
-
-			echo -n  res${s2} "">> rescoring.txt
-			grep  minimizedAffinity res${s2}|sed -n '1p'|awk '{print $3}'>>rescoring.txt
-
-		done
+echo "3)重新評估以得更可靠結果："
+rm -f sortDocking.txt
+sort -o sortDocking.txt -gk2 dockingAffinity.txt
+rm -f rescoring.txt
+top=$(less sortDocking.txt|tail -n 3|awk '{print $1}')
+for r in protein*.pdbqt;do
+	for s in $top;do
+		r1=`basename ${r} .pdbqt`
+		s1=$(echo ${s:3})
+		s2=${r1}${s1}
+		sed -i  '/MODEL/d' ${s}
+		sed -i '/ENDMDL/d' ${s}
+	
+		echo "rescoring $r ${s} "
+		smina --receptor $r --ligand ${s} --center_x $cx --center_y $cy --center_z $cz --size_x 20 --size_y 20 --size_z 20 --num_modes 1 --scoring vinardo --score_only --out res${s2}   1>/dev/null
+		echo -n  res${s2} "">> rescoring.txt
+		grep  minimizedAffinity res${s2}|sed -n '1p'|awk '{print $3}'>>rescoring.txt
 	done
-
-fi
+done
 
 #----------------------------------------------------------------
-
 #4)視覺化檢視3個最佳結果
 echo "4)視覺化檢視3個最佳結果："
 rm -f sortRescore.txt
